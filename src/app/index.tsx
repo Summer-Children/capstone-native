6ffcfe7c398623517f8a2f345f0a2457dbd690a7
rasulomaroff <rasulomaroff@gmail.com>
2025-02-12 01:38:28 -0800

    fix: fixes podfile lock dependencies

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,24 @@
-import { ApolloProvider } from "@apollo/client";
-import { apolloClient } from "../../src/shared/api/clients";
-import React from "react";
-import FileViewer from "../features/view-file/ui/file-viewer";
+import { ApolloProvider } from '@apollo/client'
+import { apolloClient } from '../../src/shared/api/clients'
+import React, { ReactNode } from 'react'
+import FileViewer from '../features/view-file/ui/file-viewer'
 
-const AppContent = () => { 
-  return (
-    <>
-      <FileViewer fetchedFileUrl="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" fileType="pdf" buttonText="View Pdf File" />
-      <FileViewer fetchedFileUrl="https://docs.google.com/spreadsheets/d/1XMUUFjKqiHFlBKw64fKHoF5TbCYVAXAY/export?format=xlsx" fileType="excel" buttonText="View Excel File" />
-    </>
-  );
-};
+const AppContent = (): ReactNode => {
+    return (
+        <>
+            <FileViewer
+                fetchedFileUrl="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+                fileType="pdf"
+                buttonText="View Pdf File"
+            />
+            <FileViewer
+                fetchedFileUrl="https://docs.google.com/spreadsheets/d/1XMUUFjKqiHFlBKw64fKHoF5TbCYVAXAY/export?format=xlsx"
+                fileType="excel"
+                buttonText="View Excel File"
+            />
+        </>
+    )
+}
 
 export default function App(): ReactNode {
     return (
